0247842f1d0e6987e237daa69dea2d0eeaa376fd
lararr <81035151+lararr@users.noreply.github.com>
2024-10-24 09:55:27 +0200

    add assignment 1

---
+++ Exercise_01/Lara_Rodrigues_Ristic/script.js
@@ -0,0 +1,288 @@
+
+// Keyboard//
+const aButton = document.getElementById('a-button');
+const bButton = document.getElementById('b-button');
+const cButton = document.getElementById('c-button');
+const dButton = document.getElementById('d-button');
+const eButton = document.getElementById('e-button');
+const fButton = document.getElementById('f-button');
+const gButton = document.getElementById('g-button');
+const hButton = document.getElementById('h-button');
+const iButton = document.getElementById('i-button');
+const jButton = document.getElementById('j-button');
+const kButton = document.getElementById('k-button');
+const lButton = document.getElementById('l-button');
+const mButton = document.getElementById('m-button');
+const nButton = document.getElementById('n-button');
+const oButton = document.getElementById('o-button');
+const pButton = document.getElementById('p-button');
+const qButton = document.getElementById('q-button');
+const rButton = document.getElementById('r-button');
+const sButton = document.getElementById('s-button');
+const tButton = document.getElementById('t-button');
+const uButton = document.getElementById('u-button');
+const vButton = document.getElementById('v-button');
+const wButton = document.getElementById('w-button');
+const xButton = document.getElementById('x-button');
+const yButton = document.getElementById('y-button');
+const zButton = document.getElementById('z-button');
+
+// Special letters //
+const aAccentButton = document.getElementById('a-accent-button');
+const aGraveButton = document.getElementById('a-grave-button');
+const aUmlautButton = document.getElementById('a-umlaut-button');
+const aRingButton = document.getElementById('a-ring-button');
+const aeLigatureButton = document.getElementById('ae-ligature-button');
+const cCedillaButton = document.getElementById('c-cedilla-button');
+const cCaronButton = document.getElementById('c-caron-button');
+const cAcuteButton = document.getElementById('c-acute-button');
+const ethButton = document.getElementById('eth-button');
+const dzButton = document.getElementById('dz-button');
+const eAccentButton = document.getElementById('e-accent-button');
+const eGraveButton = document.getElementById('e-grave-button');
+const iAccentButton = document.getElementById('i-accent-button');
+const iGraveButton = document.getElementById('i-grave-button');
+const nTildeButton = document.getElementById('n-tilde-button');
+const oAccentButton = document.getElementById('o-accent-button');
+const oGraveButton = document.getElementById('o-grave-button');
+const oUmlautButton = document.getElementById('o-umlaut-button');
+const sCaronButton = document.getElementById('s-caron-button');
+const uAccentButton = document.getElementById('u-accent-button');
+const uGraveButton = document.getElementById('u-grave-button');
+const uUmlautButton = document.getElementById('u-umlaut-button');
+const yAccentButton = document.getElementById('y-accent-button');
+const zCaronButton = document.getElementById('z-caron-button');
+const thornButton = document.getElementById('thorn-button');
+const sharpSButton = document.getElementById('s-sharp-button');
+const oeLigatureButton = document.getElementById('oe-ligature-button');
+
+// Output //
+const outputParagraph = document.getElementById('output');
+
+// Delete button //
+const deleteButton = document.getElementById('delete-button');
+
+
+
+// Keyboard output //
+aButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🥑";
+})
+
+bButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🐝";
+})
+
+cButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "👑";
+})
+
+dButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🐕";
+})
+
+eButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🦅";
+});
+
+fButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🌸"; 
+});
+
+gButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🍇"; 
+});
+
+hButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🏠"; 
+});
+
+iButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🍦"; 
+});
+
+jButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🪼"; 
+});
+
+kButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🔑"; 
+});
+
+lButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🍋"; 
+});
+
+mButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🍉"; 
+});
+
+nButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🎵"; 
+});
+
+oButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🍊"; 
+});
+
+pButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🍕"; 
+});
+
+qButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "👸"; 
+});
+
+rButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🌈"; 
+});
+
+sButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🌟"; 
+});
+
+tButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🐅"; 
+});
+
+uButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "☂️"; 
+});
+
+vButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🎻"; 
+});
+
+wButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🌎"; 
+});
+
+xButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "❌"; 
+});
+
+yButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "👶"; 
+});
+
+zButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "⚡"; 
+});
+
+
+// Special letters output //
+aAccentButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🐊"; 
+});
+
+aGraveButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🎭"; 
+});
+
+aUmlautButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🛩"; 
+});
+
+aRingButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🤸‍♂️"; 
+});
+
+aeLigatureButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "👨🏻‍🎨"; 
+});
+
+cCedillaButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "☕︎"; 
+});
+
+cCaronButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "✨"; 
+});
+
+cAcuteButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "⚱️"; 
+});
+
+ethButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🚮"; 
+});
+
+dzButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🫚"; 
+});
+
+eAccentButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🐘"; 
+});
+
+eGraveButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🍽️"; 
+});
+
+iAccentButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🍓"; 
+});
+
+iGraveButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🥭"; 
+});
+
+nTildeButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🦖"; 
+});
+
+oAccentButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🌾"; 
+});
+
+oGraveButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🍈"; 
+});
+
+oUmlautButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🫒"; 
+});
+
+sCaronButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🌳"; 
+});
+
+uAccentButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🍹"; 
+});
+
+uGraveButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🚙"; 
+});
+
+uUmlautButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "👊"; 
+});
+
+yAccentButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🏎️"; 
+});
+
+zCaronButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🔥"; 
+});
+
+thornButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🍿"; 
+});
+
+sharpSButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🧊"; 
+});
+
+oeLigatureButton.addEventListener('click', () => { 
+    outputParagraph.innerText += "🥟"; 
+});
+
+
+
+// Delete button //
+deleteButton.addEventListener('click', () => {
+    outputParagraph.innerText = '';
+});
+
